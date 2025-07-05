78f13ddb32d36bf7911f3b3999ef3675392b9415
Kunal Sethi <sethikunal0108@gmail.com>
2025-07-05 19:37:56 -0400

    Edited the setup for react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 import "./App.css";
 import ProductDetails from "./pages/ProductDetails";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Signup from "./pages/Auth/Signup";
 import Signin from "./pages/Auth/Signin";
@@ -10,31 +10,38 @@ import ForgotPassword from "./pages/Auth/ForgotPassword";
 import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
 import Layout from "./components/Layout/Layout";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { checkAuthStatus } from "./redux/features/auth/authThunkActions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuthStatus()).unwrap().catch();
+  }, [dispatch]);
+
   return (
     <>
-      <Router>
-        <Layout>
-          <Routes>
-            {/* Auth Routes */}
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Layout>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
 
-            {/* Main Pages Routes */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product" element={<ProductDetails />} />
-            <Route path="/category">
-              <Route path="grocery" element={<ProductDetails />} />
-            </Route>
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/profile" element={<Profile />} />
+          {/* Main Pages Routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product" element={<ProductDetails />} />
+          <Route path="/category">
+            <Route path="grocery" element={<ProductDetails />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/profile" element={<Profile />} />
 
-            {/* Admin Routes */}
-          </Routes>
-        </Layout>
-      </Router>
+          {/* Admin Routes */}
+        </Routes>
+      </Layout>
     </>
   );
 }
